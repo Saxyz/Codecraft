4b2adda0eb655bf1c9677a859695b1fdfaeee401
Juan Avendaño <juanandreslujan30@gmail.com>
2024-04-15 06:38:07 -0500

    Arreglado problemas con el servidor

--- conexionServidor.js
+++ conexionServidor.js
@@ -1,14 +1,14 @@
 const inputBox = document.getElementById("code-box");
 
 inputBox.addEventListener('change', () =>{
-    const codigo = inputBox.value;
+    const content = inputBox.value;
 
     fetch('http://localhost:5000/lexer', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({codigo: codigo})
+        body: JSON.stringify({content: content})
     })
     .then(response => response.json())
     .then(data=>{
